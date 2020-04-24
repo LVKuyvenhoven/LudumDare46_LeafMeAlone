dfe841886f0563b92c897e364b6cbd27a0947fa4
Lenny <lvkuyvenhoven@hotmail.com>
2020-04-24 21:35:28 +0200

    Folder Merge fix
    Added some folders holding png files.

---
+++ LudumDare-2020-April/Assets/Scripts/CloudBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehavior : MonoBehaviour
+{
+    [Header("Variables")]
+    float timeToScale;
+
+    private void Awake()
+    {
+        StartCoroutine(SizeCloud());
+    }
+
+    IEnumerator SizeCloud()
+    {
+        while (timeToScale < 1000)
+        {
+            Debug.Log(timeToScale);
+            yield return null;
+            gameObject.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+        }
+        yield return null;
+    }
+
+}
