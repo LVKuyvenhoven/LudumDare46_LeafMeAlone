75680437e4705f63f351a5f379963b5a6a85ad65
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-04-24 23:27:55 +0200

    Redo - Movement script

--- LudumDare-2020-April/Assets/Scripts/Movement_Script.cs
+++ LudumDare-2020-April/Assets/Scripts/Movement_Script.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class Movement_Script : MonoBehaviour
 {
-
     public CharacterController controller;
     public float speed = 12f;
-    public float gravity = -9.81f;
+    float gravity = -9.81f;
+
     Vector3 velocity;
+    Vector3 move;
+
+    public KeyCode Forward;
+    public KeyCode Back;
+
+    public KeyCode Right;
+    public KeyCode Left;
+
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        Movement();
+    }
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+
+    void Movement()
+    {
+        if (Input.GetKey(Forward))
+        {
+            move = transform.forward * 0.5f;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(Left))
+        {
+            move = transform.right * -0.5f;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(Back))
+        {
+            move = transform.forward * -0.5f;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(Right))
+        {
+            move = transform.right * 0.5f;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         controller.Move(velocity * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
     }
