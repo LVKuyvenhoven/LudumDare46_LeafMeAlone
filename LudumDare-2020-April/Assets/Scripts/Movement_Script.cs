c7c9fa1d4358992e57c8674f906b8de3032680a7
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-04-24 21:37:17 +0200

    WOEPS

--- LudumDare-2020-April/Assets/Scripts/Movement_Script.cs
+++ LudumDare-2020-April/Assets/Scripts/Movement_Script.cs
@@ -15,13 +15,13 @@ public class Movement_Script : MonoBehaviour
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         if (Input.GetKey(Left))
-            rb.(Vector3.left * Speed);
+            rb.AddForce(Vector3.left * Speed);
         if (Input.GetKey(Right))
-            rb.AddRelativeForce(Vector3.right * Speed);
+            rb.AddForce(Vector3.right * Speed);
 
         if (Input.GetKey(Forward))
-            rb.AddRelativeForce(Vector3.forward * Speed);
+            rb.AddForce(Vector3.forward * Speed);
         if (Input.GetKey(Backward))
-            rb.AddRelativeForce(Vector3.back * Speed);
+            rb.AddForce(Vector3.back * Speed);
     }
 }
