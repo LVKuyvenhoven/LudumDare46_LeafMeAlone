958a0b37c291055de56d46beff0bf7aaa1391523
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-04-25 10:59:43 +0200

    Scene Stephan + Model Isabelle

---
+++ LudumDare-2020-April/Assets/Scripts/PlantScriptjuh.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantScriptjuh : MonoBehaviour
+{
+    public float SunStatus = 100;
+    public float WaterStatus = 100;
+}
