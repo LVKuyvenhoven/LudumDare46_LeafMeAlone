b3b4aa08d738ce4b8b189208efa800829de3c3e5
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-04-25 17:29:15 +0200

    Sun Start

---
+++ LudumDare-2020-April/Assets/Scripts/sun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sun : MonoBehaviour {
+	public float speed;
+	float time;
+	float Curtime;
+	public float hour;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Curtime >= 360)
+		{
+			Curtime = 0;
+		}
+
+		time = speed * Time.deltaTime;
+		Curtime = Curtime + time;
+		transform.RotateAround(Vector3.zero, Vector3.right, time);
+		transform.LookAt(Vector3.zero);
+		hour = Curtime / 360.0f * 24.0f;
+	}
+}
