4bfcd4580b5b3c0770dca2a67a0f07bfa7117858
Lenny <lvkuyvenhoven@hotmail.com>
2020-04-25 14:09:20 +0200

    New Sky Prefab

--- LudumDare-2020-April/Assets/Scripts/WeatherSystem/WeatherManager.cs
+++ LudumDare-2020-April/Assets/Scripts/WeatherSystem/WeatherManager.cs
@@ -11,9 +11,10 @@ public class WeatherManager : MonoBehaviour
     [HideInInspector] public GameObject minXPositionFrame, maxXPositionFrame, minZPositionFrame, maxZPositionFrame;
 
     [Header("Weather Spawner")]
-    public int maxAmountOfCloudsAndSuns = 10;
     public int spawnRateInSeconds = 5;
+    [Range(0.5f, 2.8f)]
     public float spawnOffset;
+    [HideInInspector] public int maxAmountOfCloudsAndSuns = 10000;
 
     [Header("Sun and Cloud Stats")]
     public float growRate = 0.005f;
@@ -21,56 +22,108 @@ public class WeatherManager : MonoBehaviour
     [HideInInspector] public float maxSize;
 
     [Header("Variables")]
-    int sectorIndex, previousSectorIndex;
+    int elementsInList;
     float minXPosition, maxXPosition, minYPosition, maxYPosition;
-    float randomPositionX, randomPositionZ, previousPositionX, previousPositionZ, distanceToPreviousX, distanceToPreviousZ;
+    float randomPositionX, randomPositionZ, distanceToPreviousX, distanceToPreviousZ;
+    //float previousPositionX, previousPositionZ, 
     Vector3 newCloudPosition;
-    bool spawnCloud, spawnLocationChosen;
+    bool spawnANewObject, spawnCloud, spawnLocationChosen, foundABadPosition;
+
+    List<float> previousXPositions = new List<float>();
+    List<float> previousZPositions = new List<float>();
 
     private void Awake()
     {
         maxSize = 300;
-        StartCoroutine(SpawnCloud());
+        previousXPositions.Add(100f);
+        previousZPositions.Add(100f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!spawnANewObject)
+        {
+            spawnANewObject = true;
+            StartCoroutine(SpawnCloud());
+        }
     }
 
     IEnumerator SpawnCloud()
     {
-        for (int i = 0; i < maxAmountOfCloudsAndSuns; i++) {
-            //Get a random spawn position
-            while (!spawnLocationChosen) {
-                //Get a random position within the boundaries
-                randomPositionX = Random.Range(minXPositionFrame.transform.position.x + 3, maxXPositionFrame.transform.position.x - 3);
-                randomPositionZ = Random.Range(minZPositionFrame.transform.position.z + 3, maxZPositionFrame.transform.position.z - 3);
-                //Calculate the distance to the previous position
-                distanceToPreviousX = Mathf.Abs(randomPositionX - previousPositionX);
-                distanceToPreviousZ = Mathf.Abs(randomPositionZ - previousPositionZ);
-                if (distanceToPreviousX > spawnOffset || distanceToPreviousZ > spawnOffset) {
-                    //Pick again
-                } else {
-                    spawnLocationChosen = true;
+        //Get a random spawn position
+        while (!spawnLocationChosen)
+        {
+            //Get a random position within the boundaries
+            randomPositionX = Random.Range(minXPositionFrame.transform.position.x + 3, maxXPositionFrame.transform.position.x - 3);
+            randomPositionZ = Random.Range(minZPositionFrame.transform.position.z + 1, maxZPositionFrame.transform.position.z - 3);
+
+            foundABadPosition = false;
+            //Check all the previous X positions
+            foreach (float position in previousXPositions)
+            {
+                distanceToPreviousX = Mathf.Abs(randomPositionX - position);
+                //Check if the distance between the new position and the previous position is bigger 
+                if (distanceToPreviousX < spawnOffset)
+                {
+                    //This position is not good
+                    foundABadPosition = true;
                 }
-                yield return null;
             }
-            spawnLocationChosen = false;
-
-            newCloudPosition = new Vector3(randomPositionX, gameObject.transform.position.y, randomPositionZ);
-            previousPositionX = randomPositionX;
-            previousPositionZ = randomPositionZ;
-
-            //Determine if a cloud or a sun must be spawned
-            if (spawnCloud) {
-                spawnCloud = false;
-                newCloudPosition = new Vector3(randomPositionX, groundObject.transform.position.y + 0.55f, randomPositionZ);
-                Instantiate(cloudPrefabObject, newCloudPosition, Quaternion.Euler(90, 0, 0), gameObject.transform);
-            } else{
-                spawnCloud = true;
-                newCloudPosition = new Vector3(randomPositionX, groundObject.transform.position.y + 0.55f, randomPositionZ);
-                Instantiate(sunPrefabObject, newCloudPosition, Quaternion.Euler(90, 0, 0), gameObject.transform);
+            //Check all the previous Z positions
+            foreach (float position in previousZPositions)
+            {
+                distanceToPreviousZ = Mathf.Abs(randomPositionZ - position);
+                //Check if the distance between the new position and the previous position is bigger 
+                if (distanceToPreviousZ < spawnOffset)
+                {
+                    //This position is not good
+                    foundABadPosition = true;
+                }
             }
-            //Delay before spawning another cloud
-            yield return new WaitForSeconds(spawnRateInSeconds);
+            if (!foundABadPosition)
+            {
+                spawnLocationChosen = true;
+            }
+            yield return null;
         }
-        yield return null;
+        spawnLocationChosen = false;
+
+        newCloudPosition = new Vector3(randomPositionX, gameObject.transform.position.y, randomPositionZ);
+        previousXPositions.Add(randomPositionX);
+        previousZPositions.Add(randomPositionZ);
+
+        //Make sure the list won't be too long, so that we can keep spawning clouds and sunrays
+        elementsInList += 1;
+        if (elementsInList >= 3)
+        {
+            previousXPositions.RemoveAt(0);
+            previousZPositions.RemoveAt(0);
+            //elementsInList -= 1;
+        }
+
+        //Determine if a cloud or a sun must be spawned
+        if (spawnCloud)
+        {
+            spawnCloud = false;
+            newCloudPosition = new Vector3(randomPositionX, groundObject.transform.position.y + 0.55f, randomPositionZ);
+            Instantiate(cloudPrefabObject, newCloudPosition, Quaternion.Euler(90, 0, 0), gameObject.transform);
+        }
+        else
+        {
+            spawnCloud = true;
+            newCloudPosition = new Vector3(randomPositionX, groundObject.transform.position.y + 0.55f, randomPositionZ);
+            Instantiate(sunPrefabObject, newCloudPosition, Quaternion.Euler(90, 0, 0), gameObject.transform);
+        }
+        //Delay before spawning another cloud
+        //StartCoroutine(MakeRoomForNewCloud());
+        yield return new WaitForSeconds(spawnRateInSeconds);
+        spawnANewObject = false;
+    }
+
+    IEnumerator MakeRoomForNewCloud()
+    {
+        yield return new WaitForSeconds(idleTimeInSeconds);
+        elementsInList -= 1;
     }
 
 }
