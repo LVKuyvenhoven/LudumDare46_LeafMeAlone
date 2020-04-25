0685be124ce18079f3875cfff2e53608767ad64a
BlackLotusM <35292137+BlackLotusM@users.noreply.github.com>
2020-04-25 18:44:59 +0200

    DateTime :D

--- LudumDare-2020-April/Assets/Scripts/sun.cs
+++ LudumDare-2020-April/Assets/Scripts/sun.cs
@@ -1,12 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class sun : MonoBehaviour {
 	public float speed;
 	float time;
 	float Curtime;
 	public float hour;
+
+	public bool Twaalf_Snachts;
+	public bool Twaalf_SMiddags;
+
+	public bool Zes_Avond;
+	public bool Zes_Ochtend;
+
+	public TextMeshProUGUI timetxt;
 	
+
+	private void Start()
+	{
+		if (Twaalf_Snachts)
+		{
+			transform.position = new Vector3(0,-500,0);
+			Curtime = 360;
+		}
+		else if (Twaalf_SMiddags)
+		{
+			transform.position = new Vector3(0, 500, 0);
+			Curtime = 180;
+		}
+		else if(Zes_Avond)
+		{
+			transform.position = new Vector3(0, 0, 500);
+			Curtime = 270;
+		}
+		else if (Zes_Ochtend)
+		{
+			transform.position = new Vector3(0, 0, -500);
+			Curtime = 90;
+		}
+		else
+		{
+			transform.position = new Vector3(0, 500, 0);
+			Curtime = 180;
+		}
+
+		transform.LookAt(Vector3.zero);
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -20,5 +61,7 @@ public class sun : MonoBehaviour {
 		transform.RotateAround(Vector3.zero, Vector3.right, time);
 		transform.LookAt(Vector3.zero);
 		hour = Curtime / 360.0f * 24.0f;
+		int hours = (int)hour;
+		timetxt.text = "" + hours;
 	}
 }
