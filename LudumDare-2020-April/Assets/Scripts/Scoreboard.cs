ddc692da890f9b5720529a0944930cab90624f1d
Lenny <lvkuyvenhoven@hotmail.com>
2020-04-25 19:14:33 +0200

    Timer v1

--- LudumDare-2020-April/Assets/Scripts/Scoreboard.cs
+++ LudumDare-2020-April/Assets/Scripts/Scoreboard.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Scoreboard : MonoBehaviour
@@ -8,12 +9,16 @@ public class Scoreboard : MonoBehaviour
     public static int PlantsDead;
     public static int CurrentPlatsDead;
 
+    float timeAlive;
+
     public TextMeshProUGUI deadlabel;
+    public TextMeshProUGUI timelabel;
 
     void Start()
     {
         CurrentPlatsDead = 0;
         deadlabel.text = "" + CurrentPlatsDead;
+        timelabel.text = "00:00";
     }
 
     void Update()
@@ -23,5 +28,17 @@ public class Scoreboard : MonoBehaviour
             CurrentPlatsDead = PlantsDead;
             deadlabel.text = ""+CurrentPlatsDead;
         }
+        if (CurrentPlatsDead == 4)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        CountPlayingTime();
     }
+
+    void CountPlayingTime()
+    {
+        timeAlive += 1 * Time.deltaTime;
+        Debug.Log(timeAlive);
+    }
+
 }
