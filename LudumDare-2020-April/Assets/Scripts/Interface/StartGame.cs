32930448eb7c4d0c3267e9720ebadbbcc17f33cc
Lenny <lvkuyvenhoven@hotmail.com>
2020-04-27 18:21:01 +0200

    Menu Done

---
+++ LudumDare-2020-April/Assets/Scripts/Interface/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public string scene;
+
+    public void StartTheGame()
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene("Game");
+        Scoreboard.CurrentPlatsDead = 0;
+    }
+
+}
---
+++ LudumDare-2020-April/Assets/Scripts/Interface/SwitchControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchControls : MonoBehaviour
+{
+    public string scene;
+
+    public GameObject pcButton;
+    public GameObject playstationButton;
+    public GameObject playstationBase;
+    public GameObject pcBase;
+
+    public bool switchToPC;
+
+    public void SwitchTheControlScreen()
+    {
+        if (switchToPC)
+        {
+            pcButton.SetActive(true);
+            pcBase.SetActive(true);
+            playstationBase.SetActive(false);
+            playstationButton.SetActive(false);
+        }
+        else
+        {
+            pcButton.SetActive(false);
+            pcBase.SetActive(false);
+            playstationBase.SetActive(true);
+            playstationButton.SetActive(true);
+        }
+    }
+
+}
